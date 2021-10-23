c4af1500326190d7c3b8e2a38e167829ee8fee4e
Obi Fortune <gabrielobi.of@gmail.com>
2021-10-23 19:17:28 +0300

    added project image to project page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,7 +1,12 @@
 import React, { Fragment } from 'react';
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import { AllProjectDetails } from '../../api/queries';
-// import {usePreviewSubscription, urlFor, PortableText} from '../lib/sanity'
+import {
+  // usePreviewSubscription,
+  urlFor,
+  // PortableText
+} from '../../lib/sanity';
 import { getClient } from '../../lib/sanity.server';
 import { Project as ProjectTypes } from '../../schema';
 
@@ -23,13 +28,52 @@ const index: NextPage<Props> = props => {
   const { projects } = props;
   return (
     <>
-      {projects.map(project => {
-        return (
-          <Fragment key={project._id}>
-            <a href={`projects/${project.slug?.current}`}>{project.name}</a>
-          </Fragment>
-        );
-      })}
+      <div className="row-auto">
+        {projects.map(project => {
+          return (
+            <Fragment key={project._id}>
+              {/* <a href={`projects/${project.slug?.current}`}>{project.name}</a> */}
+              <a
+                href={`projects/${project.slug?.current}`}
+                className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white hover:bg-gray-100 border shadow-md items-center"
+              >
+                <Image
+                  className="w-75 h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                  src={
+                    urlFor(project.projectImage?.asset).width(231).url() ||
+                    'assets/project/dummyImg.png'
+                  }
+                  alt=""
+                  width="680px"
+                  height="680px"
+                />
+                <div className="p-4 flex flex-col justify-between leading-normal">
+                  <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">
+                    {project.name}
+                  </h5>
+
+                  <button
+                    data-tippy-content="Tooltip Content"
+                    data-tippy-animation="scale"
+                    type="button"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  >
+                    Default tooltip
+                  </button>
+                  <button
+                    data-tippy-content="Tooltip Content"
+                    data-tippy-animation="fade"
+                    type="button"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  >
+                    Default tooltip
+                  </button>
+                </div>
+              </a>
+            </Fragment>
+          );
+        })}
+      </div>
     </>
   );
 };
