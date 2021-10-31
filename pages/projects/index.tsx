19e1ceb76ed9a650e29799c0438234088a63c6e1
Obi Fortune <gabrielobi.of@gmail.com>
2021-10-31 22:26:07 +0200

    started work on project screen

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -26,53 +26,61 @@ type Props = UnwrapPromise<ReturnType<typeof getStaticProps>>['props'];
 
 const index: NextPage<Props> = props => {
   const { projects } = props;
+
+  const picSize = {
+    width: 1000,
+    height: 600,
+  };
   return (
     <>
-      <div className="row-auto">
-        {projects.map(project => {
-          return (
-            <Fragment key={project._id}>
-              {/* <a href={`projects/${project.slug?.current}`}>{project.name}</a> */}
-              <a
-                href={`projects/${project.slug?.current}`}
-                className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white hover:bg-gray-100 border shadow-md items-center"
-              >
-                <Image
-                  className="w-75 h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-                  src={
-                    urlFor(project.projectImage?.asset).width(231).url() ||
-                    'assets/project/dummyImg.png'
-                  }
-                  alt=""
-                  width="680px"
-                  height="680px"
-                />
-                <div className="p-4 flex flex-col justify-between leading-normal">
-                  <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">
-                    {project.name}
-                  </h5>
-
-                  <button
-                    data-tippy-content="Tooltip Content"
-                    data-tippy-animation="scale"
-                    type="button"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                  >
-                    Default tooltip
-                  </button>
-                  <button
-                    data-tippy-content="Tooltip Content"
-                    data-tippy-animation="fade"
-                    type="button"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                  >
-                    Default tooltip
-                  </button>
-                </div>
-              </a>
-            </Fragment>
-          );
-        })}
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 2xl:grid-cols-2 gap-6 justify-items-center">
+          {projects.map(project => {
+            return (
+              <Fragment key={project._id}>
+                {/* <a href={`projects/${project.slug?.current}`}>{project.name}</a> */}
+                <a
+                  href={`projects/${project.slug?.current}`}
+                  className="grid grid-cols-3 border-2 border-dashed hover:border-black w-full"
+                >
+                  <div className="col-span-2 flex justify-center">
+                    <Image
+                      src={
+                        urlFor(project.projectImage?.asset)
+                          .width(picSize.width)
+                          .height(picSize.height)
+                          .url() || 'assets/project/dummyImg.png'
+                      }
+                      width={`${picSize.width}`}
+                      height={`${picSize.height}`}
+                    />
+                  </div>
+                  <div className="p-4 flex flex-col justify-between leading-normal">
+                    <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">
+                      {project.name}
+                    </h5>
+                    <button
+                      data-tippy-content="Tooltip Content"
+                      data-tippy-animation="scale"
+                      type="button"
+                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    >
+                      Default tooltip
+                    </button>
+                    <button
+                      data-tippy-content="Tooltip Content"
+                      data-tippy-animation="fade"
+                      type="button"
+                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    >
+                      Default tooltip
+                    </button>
+                  </div>
+                </a>
+              </Fragment>
+            );
+          })}
+        </div>
       </div>
     </>
   );
