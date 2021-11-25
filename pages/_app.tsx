bbec70af01b8f0964731a95ea9ddb4e1907085c1
Obi Fortune <gabrielobi.of@gmail.com>
2021-11-25 12:45:01 +0200

    changed to arrow function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,13 @@ import '../styles/neo.scss';
 import '../styles/myStyles.scss';
 import Header from '../components/Header';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div className="h-screen w-screen p-6">
-      <Header />
-      <div className="flex">
-        <Component {...pageProps} />
-      </div>
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <div className="w-screen h-screen p-6">
+    <Header />
+    <div className="flex">
+      <Component {...pageProps} />
     </div>
-  );
-}
+  </div>
+);
+
 export default MyApp;
