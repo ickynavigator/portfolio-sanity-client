31ddcf92606f5c9b5453e2084d3ee86d39e8bff0
Obi Fortune <gabrielobi.of@gmail.com>
2021-12-06 22:05:14 +0200

    removed unnecessary styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 
 import '../styles/globals.css';
-// import '../styles/neo.scss';
-import '../styles/myStyles.scss';
 import Layout from '../components/Layout';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
