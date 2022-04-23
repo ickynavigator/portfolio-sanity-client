2613c54732b2d2b5b053e9de53341a815b646519
Obi Fortune <gabrielobi.of@gmail.com>
2022-04-23 14:22:55 +0300

    Integrated the useSanityFetch hook and added default data

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,62 +5,84 @@ import {
   FaFacebook,
   FaGithub,
   FaLinkedinIn,
+  FaStripeS,
   FaTwitter,
 } from 'react-icons/fa';
+import { AllSocialLinks } from '../api/queries';
+import { useSanityFetch } from '../hooks';
+import { SocialLink } from '../schema';
 import Tooltip from './Tooltip';
 
-// TODO
-// find a way to fetch the links data from the sanity
-const socialLinks = [
+const SocialLinksIcons = (name: string) => {
+  switch (name) {
+    case 'Facebook':
+      return <FaFacebook />;
+    case 'Github':
+      return <FaGithub />;
+    case 'LinkedIn':
+      return <FaLinkedinIn />;
+    case 'Twitter':
+      return <FaTwitter />;
+    case 'Code':
+      return <FaCode />;
+    default:
+      return <FaStripeS />;
+  }
+};
+
+const defaultSocialLink: SocialLink[] = [
   {
-    hide: false,
-    icon: <FaGithub />,
+    _type: 'socialLink',
     link: 'https://github.com/ickynavigator',
     name: 'Github',
   },
   {
-    hide: false,
-    icon: <FaTwitter />,
+    _type: 'socialLink',
     link: 'https://twitter.com/obifortunebleh',
     name: 'Twitter',
   },
   {
-    hide: false,
-    icon: <FaCode />,
+    _type: 'socialLink',
     link: 'https://github.com/',
-    name: 'Site Source Code',
+    name: 'Code',
   },
   {
-    hide: false,
-    icon: <FaLinkedinIn />,
+    _type: 'socialLink',
     link: 'https://www.linkedin.com/in/obifortune/ ',
     name: 'LinkedIn',
   },
   {
-    hide: false,
-    icon: <FaFacebook />,
+    _type: 'socialLink',
     link: 'https://www.facebook.com/obi.fortune2',
     name: 'Facebook',
   },
 ];
 
 const Footer: React.FC = () => {
+  const [socialLinks, loading] = useSanityFetch<SocialLink[]>(
+    AllSocialLinks,
+    defaultSocialLink,
+  );
+
   return (
     <footer>
       <div className="flex justify-center m-3">
-        {socialLinks.map(({ name, link, icon }) => (
-          <Link key={name} href={link} passHref>
-            <a
-              href="replace"
-              aria-label={name}
-              className="p-1 mx-2 text-3xl text-gray-500 rounded cursor-pointer hover:bg-gray-500 hover:text-white"
-            >
-              <Tooltip tip={name} tipClass="text-sm text-gray-500">
-                {icon}
-              </Tooltip>
-            </a>
-          </Link>
-        ))}
+        {' '}
+        {!loading &&
+          socialLinks &&
+          socialLinks.map(({ name, link }) => (
+            <Link key={name} href={link} passHref>
+              <a
+                href="replace"
+                aria-label={name}
+                className="p-1 mx-2 text-3xl text-gray-500 rounded cursor-pointer hover:bg-gray-500 hover:text-white"
+              >
+                <Tooltip tip={name} tipClass="text-sm text-gray-500">
+                  {SocialLinksIcons(name)}
+                </Tooltip>
+              </a>
+            </Link>
+          ))}
       </div>
     </footer>
   );
