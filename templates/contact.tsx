da90fa1313a155d268d7cf53c168d6160b78d65d
Obi Fortune <gabrielobi.of@gmail.com>
2023-03-11 17:44:22 +0100

    added dry run message

--- templates/contact.tsx
+++ templates/contact.tsx
@@ -3,8 +3,12 @@ import { ContactForms } from '../schema';
 interface IEmailContact extends ContactForms {}
 
 export const EmailContact = ({ name, email, message }: IEmailContact) => {
+  const dryrun = process.env.NODE_ENV !== 'production';
+
   return (
     <>
+      {dryrun && <>This is a dryrun</>}
+      <br />
       {name} has sent you a mail. They attached the email - {email}
       <br />
       {message}
