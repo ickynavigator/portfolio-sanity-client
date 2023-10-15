d80997d4ddb26ea316587187a9b160a26ffd927a
Obi Fortune <gabrielobi.of@gmail.com>
2023-10-15 12:17:17 +0100

    updated contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,143 +1,119 @@
+import {
+  Button,
+  Group,
+  Stack,
+  TextInput,
+  Textarea,
+  Title,
+} from '@mantine/core';
+import { useForm, zodResolver } from '@mantine/form';
 import axios from 'axios';
 import type { NextPage } from 'next';
-import { FormEvent, useEffect, useState } from 'react';
+import { useState } from 'react';
+import { z } from 'zod';
+import Alert from '../../components/Alert';
 import MetaHead from '../../components/MetaHead';
+import useAlertManager from '../../hooks/useAlertManager';
 import { postToSanity } from '../../lib/sanity';
 import type { ContactForms } from '../../schema';
-import type { Enhanced } from '../../types';
+
+const formSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  message: z.string().max(256),
+});
 
 const Index: NextPage = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const form = useForm({
+    initialValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    validate: zodResolver(formSchema),
+  });
 
-  const [formSuc, setFormSuc] = useState(false);
-  const [formErr, setFormErr] = useState(false);
+  const [formSuccess, toggleFormSuccess] = useAlertManager(false);
+  const [formError, toggleFormError] = useAlertManager(false);
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    setFormSuc(false);
-    setFormErr(false);
+  const [loading, setLoading] = useState(false);
 
-    event.preventDefault();
-    event.stopPropagation();
+  const handleSubmit = async (values: typeof form.values) => {
+    toggleFormSuccess(false);
+    toggleFormError(false);
+    setLoading(true);
 
-    if (event.currentTarget.checkValidity() === true) {
-      try {
-        const res = await postToSanity<Enhanced<ContactForms>, ContactForms>({
-          _type: 'contactForms',
-          name,
-          email,
-          message,
-        });
+    try {
+      const { name, email, message } = values;
+      const res = await postToSanity<ContactForms, ContactForms>({
+        _type: 'contactForms',
+        name,
+        email,
+        message,
+      });
 
-        if (res.status === 200) {
-          setName('');
-          setEmail('');
-          setMessage('');
-          setFormSuc(true);
+      if (res.status === 200) {
+        form.reset();
+        toggleFormSuccess(true);
 
-          await axios.post(
-            `api/mail/contact`,
-            { ...res.data },
-            { headers: { 'Content-type': 'application/json' } },
-          );
-        }
-      } catch (error) {
-        setFormErr(true);
+        await axios.post(
+          `api/mail/contact`,
+          { ...res.data },
+          { headers: { 'Content-type': 'application/json' } },
+        );
       }
+    } catch (error) {
+      toggleFormError(true);
+    } finally {
+      setLoading(false);
     }
   };
 
-  useEffect(() => {
-    if (formSuc) {
-      setTimeout(() => {
-        setFormSuc(false);
-      }, 5000);
-    }
-  });
-
-  useEffect(() => {
-    if (formErr) {
-      setTimeout(() => {
-        setFormErr(false);
-      }, 5000);
-    }
-  });
   return (
     <>
       <MetaHead title="Contact Obi Fortune" />
-      <div className="my-3">
-        <div className="flex flex-col items-center">
-          <h1 className="text-3xl font-semibold">Contact Me</h1>
-          {formSuc && (
-            <div className="flex items-center justify-center w-full py-1 mt-3 text-green-800 bg-green-200 border-2 border-green-400 rounded">
-              Message Sent Successfully
-            </div>
-          )}
-          {formErr && (
-            <div className="flex items-center justify-center w-full py-1 mt-3 text-red-800 bg-red-200 border-2 border-red-400 rounded">
-              There was an issue please try again
-            </div>
-          )}
-          <form
-            method="POST"
-            onSubmit={handleSubmit}
-            className="flex flex-col items-center w-full mt-3 md:w-4/5"
-          >
-            <div className="inputGroup">
-              <label htmlFor="Name">Name</label>
-              <input
-                required
-                minLength={5}
-                className="inputBox"
-                type="text"
-                name="Name"
-                id="Name"
-                placeholder="Enter Name"
-                value={name}
-                onChange={e => setName(e.target.value)}
-              />
-            </div>
+      {formSuccess && <Alert.Success>Message Sent Successfully</Alert.Success>}
+      {formError && (
+        <Alert.Error>There was an issue please try again</Alert.Error>
+      )}
+      <form onSubmit={form.onSubmit(values => handleSubmit(values))}>
+        <Title order={2} mb="sm" align="right">
+          Contact Me
+        </Title>
 
-            <div className="inputGroup">
-              <label htmlFor="Email">Email</label>
-              <input
-                required
-                minLength={5}
-                className="inputBox"
-                type="text"
-                name="Email"
-                id="Email"
-                placeholder="Enter Email"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-              />
-            </div>
-
-            <div className="inputGroup">
-              <label htmlFor="Message">Message</label>
-              <textarea
-                required
-                className="inputBox"
-                name="Message"
-                id="Message"
-                rows={5}
-                minLength={10}
-                maxLength={300}
-                placeholder="Enter Message"
-                value={message}
-                onChange={e => setMessage(e.target.value)}
-              />
-            </div>
-
-            <input
+        <Stack align="stretch">
+          <TextInput
+            withAsterisk
+            label="Name"
+            placeholder="Enter Name"
+            {...form.getInputProps('name')}
+          />
+          <TextInput
+            withAsterisk
+            label="Email"
+            placeholder="Enter Email"
+            {...form.getInputProps('email')}
+          />
+          <Textarea
+            withAsterisk
+            autosize
+            minRows={4}
+            label="Message"
+            placeholder="Enter Message"
+            {...form.getInputProps('message')}
+          />
+          <Group position="right">
+            <Button
               type="submit"
-              className="mt-3 text-gray-500 border border-gray-500 rounded btn hover:bg-gray-500 hover:text-white"
-              value="Send"
-            />
-          </form>
-        </div>
-      </div>
+              color="gray"
+              variant="outline"
+              loading={loading}
+            >
+              Send
+            </Button>
+          </Group>
+        </Stack>
+      </form>
     </>
   );
 };
