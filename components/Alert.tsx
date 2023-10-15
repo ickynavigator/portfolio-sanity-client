d389a20815ad96caf7f2cc721703a804ebbbcee8
Obi Fortune <gabrielobi.of@gmail.com>
2023-10-15 12:16:47 +0100

    created alert helpers

---
+++ components/Alert.tsx
@@ -0,0 +1,42 @@
+import { Alert as MantineAlert } from '@mantine/core';
+import {
+  IconAlertCircleFilled,
+  IconCircleCheckFilled,
+} from '@tabler/icons-react';
+
+interface AlertProps {
+  children: React.ReactNode;
+}
+
+export const SuccessAlert = ({ children }: AlertProps) => {
+  return (
+    <MantineAlert
+      icon={<IconCircleCheckFilled />}
+      title="Success!"
+      color="green"
+      my="sm"
+    >
+      {children}
+    </MantineAlert>
+  );
+};
+
+export const ErrorAlert = ({ children }: AlertProps) => {
+  return (
+    <MantineAlert
+      icon={<IconAlertCircleFilled />}
+      title="Success!"
+      color="red"
+      my="sm"
+    >
+      {children}
+    </MantineAlert>
+  );
+};
+
+const Alert = {
+  Success: SuccessAlert,
+  Error: ErrorAlert,
+};
+
+export default Alert;
