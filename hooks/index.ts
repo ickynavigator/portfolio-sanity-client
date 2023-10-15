f20337d44af74b8a21d1c749d9440bf5e12f8da8
Obi Fortune <gabrielobi.of@gmail.com>
2023-10-15 17:53:52 +0100

    removed unused hooks

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,7 +1,5 @@
 // https://usehooks.com
-import useDarkMode from './useDarkMode';
-import useLocalStorage from './useLocalStorage';
-import useMedia from './useMedia';
+import useAlertManager from './useAlertManager';
 import useSanityFetch from './useSanityFetch';
 
-export { useDarkMode, useLocalStorage, useMedia, useSanityFetch };
+export { useAlertManager, useSanityFetch };
