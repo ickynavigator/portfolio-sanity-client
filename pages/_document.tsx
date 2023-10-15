3bb6075235f92675275d623af1ca346beafc7db8
Obi Fortune <gabrielobi.of@gmail.com>
2023-10-15 23:33:25 +0100

    added document for SSR

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,20 @@
-import { Head, Html, Main, NextScript } from 'next/document';
-import React from 'react';
+import { createGetInitialProps } from '@mantine/next';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-const Document: React.FC = () => {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+const getInitialProps = createGetInitialProps();
 
-export default Document;
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
