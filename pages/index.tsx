d005473cf860f3e36eec6c311f48f4832f323187
Obi Fortune <gabrielobi.of@gmail.com>
2023-10-15 17:12:53 +0100

    converted home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Alert, Center, Group, Stack, Text, Title } from '@mantine/core';
 import type { NextPage } from 'next';
 import Image from 'next/image';
 import { FaCheck, FaHourglassHalf, FaTimes } from 'react-icons/fa';
@@ -33,60 +34,58 @@ const Home: NextPage<Props> = props => {
     switch (jobStatus) {
       case 'inbetween':
         message = 'Kind of available at the moment';
-        color = 'bg-yellow-200 border-yellow-400 text-yellow-800';
+        color = 'yellow';
         icon = <FaHourglassHalf />;
         break;
       case 'close':
         message = 'Not available for hire at the moment';
-        color = 'bg-red-200 border-red-400 text-red-800';
+        color = 'red';
         icon = <FaTimes />;
         break;
       case 'open':
       default:
         message = 'Available for hire';
-        color = 'bg-green-200 border-green-400 text-green-800';
+        color = 'green';
         icon = <FaCheck />;
         break;
     }
 
     return { message, color, icon };
   })();
-  const myImage = urlFor(image).url();
-  // TODO
-  // set fall back image
-  const fallBackImage = '';
 
   return (
     <>
       <MetaHead title="Home" />
-      <div className="flex flex-col justify-center py-3">
-        <div className="flex justify-center my-2">
-          <Image
-            className="flex overflow-hidden rounded-full"
-            src={myImage !== null ? myImage : fallBackImage}
-            alt={name}
-            {...picSize}
-          />
-        </div>
-        <div className="flex flex-col items-center">
-          <h1 className="text-3xl font-semibold">{name}</h1>
-          <hr className="w-3/12 my-3" />
-          <h2 className="text-2xl font-semibold text-gray-500">{title}</h2>
-        </div>
+      <Stack align="center" spacing="xs">
+        <Center
+          sx={{
+            overflow: 'hidden',
+            borderRadius: '50%',
+          }}
+        >
+          <Image src={urlFor(image)} alt={name} priority {...picSize} />
+        </Center>
+
+        <Title order={1}>{name}</Title>
+        <Title order={2} c="dimmed">
+          {title}
+        </Title>
         {jobVisibility && (
           <>
-            <h3 className="text-xl font-semibold text-gray-500 text-center">
+            <Title order={3} c="dimmed">
               Job Status
-            </h3>
-            <div
-              className={`${jobStatusCheck.color} flex justify-center items-center rounded border-2`}
-            >
-              {jobStatusCheck.icon}
-              <span className="ml-4">{jobStatusCheck.message}</span>
-            </div>
+            </Title>
+            <Alert color={jobStatusCheck.color}>
+              <Center>
+                <Group>
+                  {jobStatusCheck.icon}
+                  <Text>{jobStatusCheck.message}</Text>
+                </Group>
+              </Center>
+            </Alert>
           </>
         )}
-      </div>
+      </Stack>
     </>
   );
 };
