19f5bd85cd674a286b4be6550f7cbddead5d4dd2
Obi Fortune <gabrielobi.of@gmail.com>
2024-01-31 18:07:58 -0500

    ignore form if empty

--- src/lib/actions/mail.ts
+++ src/lib/actions/mail.ts
@@ -2,14 +2,24 @@
 
 import { render } from '@react-email/render';
 import Mail from 'nodemailer/lib/mailer';
+import server from '~/env/server.mjs';
 import transporter from '~/lib/mail';
 import projectConfig from '~/lib/project.config';
 import { ContactForms } from '~/schema';
 import { EmailContact } from '~/templates';
 
 export const sendMail = async (emailHtml: string, options?: Mail.Options) => {
+  if (!projectConfig.mailInfo.from || !projectConfig.mailInfo.to) return;
+
+  if (
+    server.SMTP_USER === undefined ||
+    server.SMTP_PASS === undefined ||
+    server.SMTP_SERVICE === undefined
+  )
+    return;
+
   try {
-    return transporter.sendMail({
+    transporter.sendMail({
       from: projectConfig.mailInfo.from,
       to: projectConfig.mailInfo.to,
       html: emailHtml,
@@ -18,10 +28,11 @@ export const sendMail = async (emailHtml: string, options?: Mail.Options) => {
   } catch (error) {
     // TODO: Log error
     console.error(error);
-    return error;
   }
 };
 
 export const mailContactForm = async (values: ContactForms) => {
-  return sendMail(render(EmailContact(values)));
+  sendMail(render(EmailContact(values)), {
+    subject: `New Contact Form from ${values.name}`,
+  });
 };
