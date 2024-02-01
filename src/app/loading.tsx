b74b2b34112dc4ffd9e505e6b799000d92a21fe2
Obi Fortune <gabrielobi.of@gmail.com>
2024-01-31 19:59:30 -0500

    add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import { Center, Loader } from '@mantine/core';
+
+const Loading = () => {
+  return (
+    <Center h="100%">
+      <Loader size={128} />
+    </Center>
+  );
+};
+
+export default Loading;
