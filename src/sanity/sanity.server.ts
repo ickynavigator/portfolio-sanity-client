11193a67a3424663bad95bbe2c58fa238dca5b07
Obi Fortune <gabrielobi.of@gmail.com>
2024-02-02 14:05:02 -0500

    rewrite client setup

--- src/sanity/sanity.server.ts
+++ src/sanity/sanity.server.ts
@@ -1,16 +1,18 @@
-// lib/sanity.server.js
 import { createClient } from 'next-sanity';
 import server from '../env/server.mjs';
 import { config } from './sanity.config';
 
-// Set up a preview client with serverless authentication for drafts
+/** Set up a preview client with serverless authentication for drafts */
 export const previewClient = createClient({
-  ...config,
+  projectId: config.projectId,
+  dataset: config.dataset,
+  apiVersion: config.apiVersion,
   useCdn: false,
+  perspective: 'published',
   token: server.SANITY_API_TOKEN,
 });
 
-// Helper function for easily switching between normal client and preview client
+/** Helper function for easily switching between normal client and preview client */
 export const getClient = () => {
   return previewClient;
 };
