d27594c6490ad21c9b73d0fbc43cd79ce90cfb6c
Obi Fortune <gabrielobi.of@gmail.com>
2024-02-01 21:12:55 -0500

    remove base viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,7 @@ import '@mantine/core/styles.css';
 import './layout.styles.css';
 
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
-import { Viewport } from 'next';
 import theme from '~/lib/mantine.config';
-
-export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  minimumScale: 1,
-  userScalable: false,
-};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
