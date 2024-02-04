408f42dbf22b9ad1bb4a31857dc2a34dac672ca7
Obi Fortune <gabrielobi.of@gmail.com>
2024-02-04 01:35:42 -0500

    metadata and sitemap info

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,23 +2,69 @@ import '@mantine/carousel/styles.css';
 import '@mantine/core/styles.css';
 
 import { Container, Stack } from '@mantine/core';
-import { Metadata } from 'next';
+import { Metadata, ResolvingMetadata } from 'next';
 import Footer from '~/components/Footer';
 import Header from '~/components/Header';
+import { ProfileDetails } from '~/groq/queries';
 import projectConfig from '~/lib/project.config';
+import { urlForImage } from '~/sanity/sanity.lib';
+import { getClient } from '~/sanity/sanity.server';
+import { PersonalInfo } from '~/schema';
 
-export const metadata: Metadata = {
-  title: {
-    template: `%s | ${projectConfig.name}'s Personal Portfolio`,
-    default: `${projectConfig.name}'s Personal Portfolio`,
-  },
-  keywords: ['PORTFOLIO', 'DEVELOPER', 'NEXTJS', 'REACTJS', 'SANITY'],
-  robots: 'index, follow',
-  description: `${projectConfig.name}'s Personal Portfolio. Built with Next.js and Sanity.`,
+interface PersonalInfoResponse extends PersonalInfo {}
 
-  creator: 'Obi Fortune',
-  authors: [{ name: 'Obi Fortune', url: 'https://obifortune.com' }],
-};
+export async function generateMetadata(
+  _: undefined,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const client = getClient();
+  const data = await client.fetch<PersonalInfoResponse>(ProfileDetails);
+
+  const img = urlForImage(data.image);
+  const ogImages = (await parent).openGraph?.images || [];
+  const twitterImages = (await parent).twitter?.images || [];
+
+  if (img) {
+    const metaImg = { url: img, width: 160, height: 160, alt: data.title };
+
+    ogImages.unshift(metaImg);
+    twitterImages.unshift({ url: img, alt: data.title });
+  }
+
+  return {
+    title: {
+      template: `%s | ${projectConfig.name}'s Portfolio`,
+      default: `${projectConfig.name}'s Portfolio`,
+    },
+    keywords: ['PORTFOLIO', 'DEVELOPER', 'NEXTJS', 'REACTJS', 'SANITY'],
+    robots: 'index, follow',
+    description: `${projectConfig.name}'s Portfolio. Built with Next.js and Sanity.`,
+    creator: 'Obi Fortune',
+    authors: [{ name: 'Obi Fortune', url: 'https://obifortune.com' }],
+    openGraph: {
+      type: 'website',
+      title: {
+        template: `%s | ${projectConfig.name}'s Portfolio`,
+        default: `${projectConfig.name}'s Portfolio`,
+      },
+      description: `${projectConfig.name}'s Portfolio. Built with Next.js and Sanity.`,
+      url: `/`,
+      images: ogImages,
+    },
+    twitter: {
+      card: 'player',
+      title: {
+        template: `%s | ${projectConfig.name}'s Portfolio`,
+        default: `${projectConfig.name}'s Portfolio`,
+      },
+      description: `${projectConfig.name}'s Portfolio. Built with Next.js and Sanity.`,
+      creator: '@obifortunebleh',
+      creatorId: '1467726470533754880',
+      site: `/`,
+      images: twitterImages,
+    },
+  };
+}
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
---
+++ src/app/sitemap.ts
@@ -0,0 +1,47 @@
+import { MetadataRoute } from 'next';
+
+const getBaseURL = () => {
+  const _base = process.env.NEXT_PUBLIC_VERCEL_URL || 'https://localhost:3000';
+
+  const baseURL = new URL(_base);
+  baseURL.protocol = 'https';
+
+  return baseURL;
+};
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseURL = getBaseURL().toString();
+
+  return [
+    {
+      url: `${baseURL}`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${baseURL}career`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseURL}certificates`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: `${baseURL}contact`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${baseURL}projects`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ];
+}
