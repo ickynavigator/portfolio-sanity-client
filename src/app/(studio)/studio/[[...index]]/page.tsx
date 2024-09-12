05c1b54df00e3d7cff84733b011ce8ed2999daaa
Obi Fortune <gabrielobi.of@gmail.com>
2024-09-12 00:21:01 -0400

    update studio config

--- src/app/(studio)/studio/[[...index]]/page.tsx
+++ src/app/(studio)/studio/[[...index]]/page.tsx
@@ -1,23 +1,10 @@
-import type { Metadata, Viewport } from 'next';
-import {
-  metadata as studioMetadata,
-  viewport as studioViewport,
-} from 'next-sanity/studio';
-
 import Studio from '~/app/(studio)/studio/[[...index]]/studio';
 
-// Set the right `viewport`, `robots` and `referer` meta tags
-export const metadata: Metadata = {
-  ...studioMetadata,
-  // Overrides the title until the Studio is loaded
-  title: 'Loading Studio…',
-};
+// Extend serverless function timeout
+export const maxDuration = 60;
 
-export const viewport: Viewport = {
-  ...studioViewport,
-  // Overrides the viewport to resize behavior
-  interactiveWidget: 'resizes-content',
-};
+// Set the right `viewport`, `robots` and `referer` meta tags
+export { metadata, viewport } from 'next-sanity/studio';
 
 const Page = () => {
   return <Studio />;
